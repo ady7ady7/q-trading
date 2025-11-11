d1cdd8dd90e4138de43d45800a516d5e0daa05db
ady7ady7 <adrianoovv2@gmail.com>
2025-11-11 22:16:01 +0100

    Added config.py to the shared/ folder to account for the default tz for each instrument + updated .gitignore

---
+++ shared/config.py
@@ -0,0 +1,13 @@
+MARKET_TIMEZONES = {
+# Indices
+'DE40': 'Europe/Berlin',
+'US30': 'America/New_York',
+'US100': 'America/New_York',
+'JP225': 'Asia/Tokyo',
+# Forex (major sessions)
+'EURUSD': 'Europe/London',
+'USDJPY': 'America/New_York',
+'GBPUSD': 'Europe/London',
+# Default
+'DEFAULT': 'UTC'
+}
